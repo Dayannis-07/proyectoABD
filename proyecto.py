b6f0bf6e248135d429c2fc1ca468734eb40dc4b7
Dayannis <dayanniscolmenaresmarquina@gmail.com>
2026-06-13 16:37:43 -0400

    se agregaron las consultas de genesis

--- proyecto.py
+++ proyecto.py
@@ -167,6 +167,36 @@ else:
                 )
                 cursor = conn.cursor()
 
+                
+
+                mostrar_tabla_con_encabezados(
+                cursor,
+                 """
+                SELECT
+                    s.name AS Esquema,
+                    t.name AS Tabla,
+                    c.name AS NombreRestriccion,
+                    CASE c.type
+                        WHEN 'PK' THEN 'PRIMARY KEY'
+                        WHEN 'F' THEN 'FOREIGN KEY'
+                        WHEN 'C' THEN 'CHECK'
+                        WHEN 'UQ' THEN 'UNIQUE'
+                        WHEN 'D' THEN 'DEFAULT'
+                    END AS TipoRestriccion
+                FROM sys.objects c
+                INNER JOIN sys.tables t
+                    ON c.parent_object_id = t.object_id
+                INNER JOIN sys.schemas s
+                    ON t.schema_id = s.schema_id
+                WHERE s.name = 'streaming'
+                    AND c.type IN ('PK','F','C','UQ','D')
+                ORDER BY
+                    t.name,
+                    TipoRestriccion,
+                    c.name
+                """,
+                "Restricciones del esquema",)
+
                 conn.close()
             except Exception as e:
                 st.error(f"Error técnico: {e}")
@@ -182,6 +212,82 @@ else:
                     creds["driver"],
                 )
                 cursor = conn.cursor()
+
+
+                mostrar_tabla_con_encabezados(
+                cursor,
+                """
+                SELECT
+                t.name AS Tabla,
+                COUNT(i.index_id) AS CantidadIndices
+                FROM sys.tables t
+                INNER JOIN sys.schemas s
+                ON t.schema_id = s.schema_id
+                LEFT JOIN sys.indexes i
+                ON t.object_id = i.object_id
+                AND i.name IS NOT NULL
+                WHERE s.name = 'streaming'
+                GROUP BY t.name
+                ORDER BY CantidadIndices DESC, t.name
+                """,
+                "Cantidad de Índices por Tabla",
+                )
+
+                
+
+                mostrar_tabla_con_encabezados(
+                    cursor,
+                    """
+                    SELECT
+                        t.name AS Tabla,
+                        i.name AS NombreIndice,
+                        STRING_AGG(c.name, ', ')
+                            WITHIN GROUP (ORDER BY ic.key_ordinal) AS Columnas,
+                        CASE
+                            WHEN i.is_unique = 1 THEN 'Sí'
+                            ELSE 'No'
+                        END AS EsUnico,
+                        i.type_desc AS TipoIndice,
+                        CASE
+                            WHEN i.is_primary_key = 1 THEN 'Sí'
+                            ELSE 'No'
+                        END AS EsPrimaryKey,
+                        CASE
+                            WHEN i.is_unique_constraint = 1 THEN 'Sí'
+                            ELSE 'No'
+                        END AS EsRestriccionUnique,
+                        CASE
+                            WHEN i.is_disabled = 1 THEN 'Sí'
+                            ELSE 'No'
+                        END AS EstaDeshabilitado
+                    FROM sys.indexes i
+                    INNER JOIN sys.tables t
+                        ON i.object_id = t.object_id
+                    INNER JOIN sys.schemas s
+                        ON t.schema_id = s.schema_id
+                    INNER JOIN sys.index_columns ic
+                        ON i.object_id = ic.object_id
+                        AND i.index_id = ic.index_id
+                    INNER JOIN sys.columns c
+                        ON ic.object_id = c.object_id
+                        AND ic.column_id = c.column_id
+                    WHERE s.name = 'streaming'
+                        AND i.name IS NOT NULL
+                    GROUP BY
+                        t.name,
+                        i.name,
+                        i.is_unique,
+                        i.type_desc,
+                        i.is_primary_key,
+                        i.is_unique_constraint,
+                        i.is_disabled
+                    ORDER BY
+                        t.name,
+                        i.name
+                    """,
+                    "Detalle Índices del Esquema",
+                )
+
 
                 conn.close()
             except Exception as e:
@@ -199,6 +305,35 @@ else:
                 )
                 cursor = conn.cursor()
 
+                
+
+                mostrar_tabla_con_encabezados(
+                    cursor,
+                    """
+                    SELECT
+                        tr.name AS NombreTrigger,
+                        tb.name AS Tabla,
+                        CASE tr.type
+                            WHEN 'TR' THEN 'SQL_TRIGGER'
+                            ELSE tr.type_desc
+                        END AS TipoTrigger,
+                        CASE
+                            WHEN tr.is_disabled = 1 THEN 'Deshabilitado'
+                            ELSE 'Habilitado'
+                        END AS Estado
+                    FROM sys.triggers tr
+                    INNER JOIN sys.tables tb
+                        ON tr.parent_id = tb.object_id
+                    INNER JOIN sys.schemas s
+                        ON tb.schema_id = s.schema_id
+                    WHERE s.name = 'streaming'
+                    ORDER BY tb.name, tr.name
+                    """,
+                    "Info Triggers del Esquema",
+                )
+
+
+
                 conn.close()
             except Exception as e:
                 st.error(f"Error técnico: {e}")
@@ -214,6 +349,32 @@ else:
                     creds["driver"],
                 )
                 cursor = conn.cursor()
+                
+                mostrar_tabla_con_encabezados(
+                    cursor,
+                    """
+                    SELECT
+                        t.name AS Tabla,
+                        SUM(p.rows) AS Filas,
+                        SUM(a.total_pages) * 8 AS TamañoKB,
+                        CAST(SUM(a.total_pages) * 8.0 / 1024 AS DECIMAL(10,2)) AS TamañoMB
+                    FROM sys.tables t
+                    INNER JOIN sys.schemas s
+                        ON t.schema_id = s.schema_id
+                    INNER JOIN sys.indexes i
+                        ON t.object_id = i.object_id
+                    INNER JOIN sys.partitions p
+                        ON i.object_id = p.object_id
+                        AND i.index_id = p.index_id
+                    INNER JOIN sys.allocation_units a
+                        ON p.partition_id = a.container_id
+                    WHERE s.name = 'streaming'
+                    GROUP BY t.name
+                    ORDER BY TamañoKB DESC
+                    """,
+                    "Tamaño ocupado por cada tabla",
+                )
+
 
                 conn.close()
             except Exception as e:
